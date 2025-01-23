f4ab710d6d347bb96f0b76d89110e863d01e1d0c
mohamedr54 <mohamedrakai25@gmail.com>
2025-01-23 19:59:34 +0700

    rewriting docstring. finishing test_transform.py

--- tests/test_transform.py
+++ tests/test_transform.py
@@ -6,20 +6,128 @@ import pytest
 import json
 from transform import *
 
+
 def test_load_json_data_to_dict():
 
     filename="./tests/temp/testjson_to_load.txt"
-    sample_to_write={"hourly": {"temperature_2m": [10, 12, 14]},
-    "daily": {"temperature_2m_max": [15, 16, 17]}
+    sample_to_write={"hourly": {"temperature_2m": [10, 12, 14],
+                                "cloud_cover": [0, 0, 3]},
     }
+
     with open(filename, "w") as file:
         json.dump(sample_to_write,file)
 
-    test_hourly_dict, test_daily_dict = load_json_data_to_dict(filename)
-    assert test_hourly_dict == {"temperature_2m": [10, 12, 14]}
-    assert test_daily_dict == {"temperature_2m_max": [15, 16, 17]}
+    my_dict = load_json_data_to_dict(filename)
+    assert my_dict == {"temperature_2m": [10, 12, 14],
+                       "cloud_cover": [0, 0, 3]}
 
 
 
-def test_example():
-    assert 1 + 1 == 2
+def test_dict_to_dataframe():
+    test_my_dict = {"temperature_2m": [10, 12, 14],
+                    "cloud_cover": [0, 0, 3]}
+    test_my_df = dict_to_dataframe(test_my_dict)
+
+    expected_df=pd.DataFrame(test_my_dict)
+    pd.testing.assert_frame_equal(test_my_df, expected_df)
+
+
+def test_drop_duplicates():
+
+    test_df = df = pd.DataFrame({
+    "time": [
+        pd.Timestamp("2024-12-17 00:00:00"), pd.Timestamp("2024-12-17 01:00:00"),
+        pd.Timestamp("2024-12-17 02:00:00"), pd.Timestamp("2024-12-17 03:00:00"),
+        pd.Timestamp("2024-12-17 00:00:00"), pd.Timestamp("2024-12-17 02:00:00")],
+    "temperature": [12, 15, 18, 20, 12, 18],
+    "cloud_cover": [0, 0, 3, 4, 0, 3]
+})
+
+
+    drop_duplicates(test_df)
+
+    expected_df = pd.DataFrame({
+            "time": [pd.Timestamp("2024-12-17 00:00:00"), pd.Timestamp("2024-12-17 01:00:00"),
+                     pd.Timestamp("2024-12-17 02:00:00"), pd.Timestamp("2024-12-17 03:00:00")],
+            "temperature": [12, 15, 18,20],"cloud_cover": [0, 0, 3,4]})
+
+    pd.testing.assert_frame_equal(test_df, expected_df)
+
+
+def dropping_nan_values(my_df: pd.DataFrame) -> pd.DataFrame:
+    """
+    This function deletes any row that contains NaN values.
+
+    NOTE: Here I made the choice to delete rows with NaN values. I could have used cluster imputation method to fill
+    some values in case of errors. However, I need to pay attention to the context: weather is kind of unpredictable,
+    so imputing data with a cluster could be meaningless. Moreover, since the data comes from a reliable website,
+    I assume that very few rows should be impacted, and deleting them will not be a problem.
+
+    :param my_df: The dataframe we got by merging hourly and daily dataframes.
+    :return: Returns a new dataframe without NaN values.
+    """
+    return my_df.dropna().reset_index(drop=True)
+
+def test_dropping_nan_values():
+    test_df = pd.DataFrame({
+        "time": [
+            pd.Timestamp("2024-12-17 00:00:00"), pd.Timestamp("2024-12-17 01:00:00"),
+            pd.Timestamp("2024-12-17 02:00:00"), pd.Timestamp("2024-12-17 03:00:00"),
+            pd.NaT],
+        "temperature": [12, 15, None, 20, None],
+        "cloud_cover": [0, 0, 3, 4, 5]
+    })
+
+    result_df = dropping_nan_values(test_df)
+
+    expected_df = pd.DataFrame({
+        "time": [pd.Timestamp("2024-12-17 00:00:00"), pd.Timestamp("2024-12-17 01:00:00"),
+                 pd.Timestamp("2024-12-17 03:00:00")],
+         "temperature": [12.0, 15.0, 20.0], #droping nan values make the column type becoming float
+        "cloud_cover": [0, 0, 4]
+    })
+
+    result_df = dropping_nan_values(test_df)
+
+    pd.testing.assert_frame_equal(result_df, expected_df)
+
+
+
+def test_formating_time_columns():
+
+        test_df = pd.DataFrame({
+            "time": ["2024-12-17T00:00", "2024-12-17T01:00", "2024-12-17T02:00", "2024-12-17T03:00"],
+            "temperature": [12, 15, 18,20],
+            "cloud_cover": [0, 0, 3,4]
+        })
+
+        formating_time_column(test_df)
+
+        expected_df = pd.DataFrame({
+            "time": [pd.Timestamp("2024-12-17 00:00:00"), pd.Timestamp("2024-12-17 01:00:00"),
+                     pd.Timestamp("2024-12-17 02:00:00"), pd.Timestamp("2024-12-17 03:00:00")],
+            "temperature": [12, 15, 18,20],
+            "cloud_cover": [0, 0, 3,4]})
+
+
+        assert test_df["time"].equals(expected_df["time"])
+
+
+
+def test_saving_to_csv(tmp_path):
+    data = pd.DataFrame({
+        "time": [pd.Timestamp("2024-12-17 00:00:00"), pd.Timestamp("2024-12-17 01:00:00")],
+        "temperature": [12, 15],
+        "cloud_cover": [0, 0]
+    })
+
+    csv_path = "./tests/temp/final.csv"
+
+    data.to_csv(csv_path, index=False) #removes the index when saving the file
+
+    saved_df = pd.read_csv(csv_path)
+
+    saved_df['time'] = pd.to_datetime(saved_df['time'])#needed because when we load the csv, date is at a str format
+
+    pd.testing.assert_frame_equal(data, saved_df)
+
--- transform.py
+++ transform.py
@@ -5,101 +5,73 @@ import json
 
 
 
-def load_json_data_to_dict(filename):
+def load_json_data_to_dict(filename:str):
     """
-    this function takee the important data we stored and loads it in two files : one four hourly metrics and
-    another one for daily metrics
+    this function take the important data we stored and loads it into a dictionary
     :param filename: name of the file we're going to extract data from
-    :return: return a tuple composed of two dicts
+    :return: returns a dict
     """
     with open(filename, "r") as file:  # extracting hourly and daily variables as proper dictionaries
         my_data = json.load(file)
-    hourly_dict = my_data['hourly']
-    daily_dict = my_data['daily']
-    return hourly_dict, daily_dict
+    my_dict = my_data['hourly']
+    return my_dict
 
 
 
-def dict_to_dataframe(hourly_dict, daily_dict):
+def dict_to_dataframe(my_dict:dict):
     """
     this function transforms each dict into a pandas dataframe
-    :param hourly_dict: hourly dict returned by load_json_data_to_dict function
-    :param daily_dict: daily dict returned by load_json_data_to_dict function
-    :return: return a tuple composed of two dataframes
+    :param my_dict:  dict returned by load_json_data_to_dict function
+    :return: return a dataframe
     """
-    hourly_df = pd.DataFrame.from_dict(hourly_dict)
-    daily_df = pd.DataFrame.from_dict(daily_dict)
-    return hourly_dict,daily_dict
+    my_df = pd.DataFrame.from_dict(my_dict)
+    return my_df
 
 
+def drop_duplicates(my_df:pd.DataFrame):
+    """
+    this function drops duplicate rows
+    :param my_df: dataframe containing weather data
+    :return: returns a dataframe with duplicate rows dropped
+    """
+    my_df.drop_duplicates(inplace=True)
+    return my_df
 
-def formating_time_columns(hourly_df, daily_df):
+
+def dropping_nan_values(my_df: pd.DataFrame) -> pd.DataFrame:
+    """
+    This function deletes any row that contains NaN values.
+
+    NOTE: Here I made the choice to delete rows with NaN values. I could have used cluster imputation method to fill
+    some values in case of errors. However, I need to pay attention to the context: weather is kind of unpredictable,
+    so imputing data with a cluster could be meaningless. Moreover, since the data comes from a reliable website,
+    I assume that very few rows should be impacted, and deleting them will not be a problem.
+
+    :param my_df: The dataframe containing weather data
+    :return: Returns a new dataframe without NaN values.
+    """
+    # Drop rows with any NaN values and reset the index
+    return my_df.dropna().reset_index(drop=True)
+
+
+def formating_time_column(my_df:pd.DataFrame ):
     """
     this function set the time column of each dataframe to a standard format. This will be necessary
     in order to merge the two dataframes.
-    :param hourly_df: hourly dataframe returned by dict_to_dataframe function
-    :param daily_df:  daily dataframe returned by dict_to_dataframe function
-    :return: returns
+    :param my_df: dataframe containing weather data
+    :return: returns the dataframe with time column set to the right format
     """
-    hourly_df["time"] = pd.to_datetime(hourly_df["time"])
-    daily_df["time"] = pd.to_datetime(daily_df["time"])
-    return hourly_df, daily_df
+    my_df["time"] = pd.to_datetime(my_df["time"])
+
+    return my_df
 
 
 
-def merging_both_dataframes(hourly_df, daily_df):
-    """
-    this function merges our both dataset into a final one, ordered by time
 
-    note :  `merge_asof` is used to merge two DataFrames with different frequencies.
-    The data must be sorted by the 'time' column before merging. This method matches each row
-    from the `hourly_df` DataFrame to the row in the `daily_df` DataFrame with the closest
-    'time' value, while preserving the chronological order.
-
-
-    :param hourly_df: dataframe with hourly metrics and a proper time column
-    :param daily_df: dataframe with daily metrics and a proper time column
-    :return:
-    """
-    final_df = pd.merge_asof(hourly_df.sort_values('time'), daily_df.sort_values('time'))
-    return final_df
-
-
-def drop_duplicates(dataframe:pd.DataFrame):
-    """
-    this function drops duplicate rows
-    :param dataframe:
-    :return: returns a dataframe with duplicate rows dropped
-    """
-    dataframe.drop_duplicates(inplace=True)
-    return dataframe
-
-
-
-def dropping_nan_values(dataframe:pd.DataFrame):
-    """
-    this function deletes any row that contains NaN values
-
-    NOTE : here I made the choice to delete rows with NaN values. I could have used cluster imputation method to fill
-    some values in case or errors. However ,I need to pay attention to the context : weather is kind of unpredictable,
-    so imputing data with a cluster could be meaningless. Moreover, since the data comes from a reliable website,
-    I assume that very few rows should be impacted, and deleting them will not be a problem.
-    :param dataframe: the dataframe we got by merging hourly and daily dataframes
-    :return: returns a new dataframe without NaN values
-    """
-    dataframe.replace(np.nan, 0, inplace=True)
-    return dataframe
-
-
-def saving_to_csv(final_df):
+def saving_to_csv(final_df : pd.DataFrame):
     """
     this function saves the final dataframe to a csv file
-    :param final_df: final dataframe
+    :param final_df: clean dataframe
     """
     final_df.to_csv("final.csv")
 
-with open("data.json", "r") as file:  # extracting hourly and daily variables as proper dictionaries
-    my_data = json.load(file)
-hourly_dict = my_data['hourly']
-daily_dict = my_data['daily']
-print(type(hourly_dict))
